d2fff174fcb80ec419e2d26b1dd90f5a76e0910c
VTAIT <vta.laptrinh@gmail.com>
2023-12-13 10:57:11 +0700

    audioPlayer - justAudio

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_utils/audio/AudioPlayerScreen.dart';
 import 'package:flutter_utils/main.dart';
 import 'package:flutter_utils/visitory/visitory_screen.dart';
 
@@ -6,5 +7,8 @@ Map<String, WidgetBuilder> routes = {
   '/': (context) => const MyHomePage(title: 'Home Page'),
   '/Visitory': (context) => const VisitoryScreen(
         title: "Visitory Pattern",
+      ),
+  '/AudioPlayer': (context) => const AudioPlayerScreen(
+        title: "AudioPlayer",
       ),
 };
